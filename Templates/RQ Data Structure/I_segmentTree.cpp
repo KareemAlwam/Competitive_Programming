d5d0cb5b1a876d113111fb332fc54672fcfbba43
kareem mohmed <kareemalwam@gmail.com>
2026-07-29 19:54:45 +0300

    Refactor segment tree implementation with Node struct

--- Templates/RQ Data Structure/I_segmentTree.cpp
+++ Templates/RQ Data Structure/I_segmentTree.cpp
@@ -1,39 +1,41 @@
-struct SegTree {// zero based
-    int n;
-    vector<long long> tree;
-
-    void init(int size) {
-        n = size;
-        tree.assign(2 * n, 0);
-    }
-
-    void build(vector<long long>& arr) {
-        for (int i = 0; i < n; i++)
-            tree[n + i] = arr[i];
-        for (int i = n - 1; i > 0; i--)
-            tree[i] = tree[2*i] + tree[2*i+1];
-    }
-
-    void update(int pos, long long val) {
-        pos += n;
-        tree[pos] = val;
-        for (pos /= 2; pos >= 1; pos /= 2)
-            tree[pos] = tree[2*pos] + tree[2*pos+1];
-    }
-
-    long long query(int l, int r) { // sum in [l, r), half-open
-        long long res = 0;
-        for (l += n, r += n; l < r; l /= 2, r /= 2) {
-            if (l & 1) res += tree[l++];
-            if (r & 1) res += tree[--r];
-        }
-        return res;
-    }
+struct Node {
+  lol ans = 0;
 };
-int main(){
+struct SegTree {
+  int n;
+  vector<Node> seg;
+  void init(int sz) { n = sz; seg.assign(2 * n, Node()); }
+  inline Node merge(const Node &a, const Node &b) {
+    Node res;
+    res.ans = a.ans + b.ans;
+    return res;
+  }
+  void build(vector<long long>& arr) {
+    for (int i = 0; i < n; i++) seg[n + i].ans = arr[i];
+    for (int i = n - 1; i; i--)
+      seg[i] = merge(seg[i << 1], seg[i << 1 | 1]);
+  }
+  void upd(int p, const Node &v) {
+    for (seg[p += n] = v; p >>= 1;)
+      seg[p] = merge(seg[p << 1], seg[p << 1 | 1]);
+  }
+  Node qry(int l, int r) {      // [l,r)
+    Node L, R;
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) L = merge(L, seg[l++]);
+      if (r & 1) R = merge(seg[--r], R);
+    }
+    return merge(L, R);
+  }
+};
+void solve() {
+  int n, q; cin >> n >> q;
+  vector<lol> arr(n); vscan(arr);
   SegTree st;
   st.init(n);
   st.build(arr);
-  st.update(3, 10);        // set index 3 to value 10
-  long long ans = st.query(2, 7); // sum of arr[2..6]
-return 0;}
+  while (q--) {
+    int l, r; cin >> l >> r;
+    cout << st.qry(l - 1, r).ans << "\n";
+  }
+}
